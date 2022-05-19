adc4618bc9ca0abfda4355f46b85f629a6c94b0d
wangsaiyu <wangsaiyu@cqu.edu.cn>
2022-05-19 12:53:08 +0800

    add check class

--- check/check.cpp
+++ check/check.cpp
@@ -1,10 +1,48 @@
 #include "check/check.h"
 
 namespace check {
-Checker::Checker(const shared_ptr<ast::FileNode>& ast_file, const shared_ptr<ec::ErrorReminder>& error_reminder)
-    : errors_(error_reminder) {}
+Checker::Checker(const shared_ptr<ast::FileNode>& file_node, const shared_ptr<ec::ErrorReminder>& error_reminder)
+    : ast_(file_node), errors_(error_reminder) {}
 
 void Checker::Check() {
-    
+    for (const auto& decl: ast_->decl_) {
+
+    }
+}
+
+/**
+ * @brief CheckRootDecl check decl in root.
+ * 
+ * @param decl ast decl node, expect var decl / func decl. 
+ */
+void Checker::CheckRootDecl(const shared_ptr<ast::DeclNode>& decl) {
+    switch (decl->Type()) {
+        case ast::VarDecl:
+            CheckVarDeclNode(dynamic_pointer_cast<ast::VarDeclNode>(decl));
+            return;
+        case ast::FuncDecl:
+            CheckFuncDeclNode(dynamic_pointer_cast<ast::FuncDeclNode>(decl));
+        default:
+            return;
+    }
+}
+
+
+/**
+ * @brief CheckVarDeclNode check var decl node in any position.
+ * 
+ * @param decl var decl node. 
+ */
+void Checker::CheckVarDeclNode(const shared_ptr<ast::VarDeclNode>& decl) {
+
+}
+
+/**
+ * @brief CheckFuncDeclNode check func decl node in root.
+ * 
+ * @param decl func decl node.
+ */
+void Checker::CheckFuncDeclNode(const shared_ptr<ast::FuncDeclNode>& decl) {
+
 }
 }
--- check/check.h
+++ check/check.h
@@ -17,6 +17,29 @@ public:
     Checker(const shared_ptr<ast::FileNode>& ast_file, const shared_ptr<ec::ErrorReminder>& error_reminder);
     void Check();
 private:
+    /**
+     * @brief CheckRootDecl check decl in root.
+     * 
+     * @param decl ast decl node, expect var decl / func decl. 
+     */
+    void CheckRootDecl(const shared_ptr<ast::DeclNode>& decl);
+
+    /**
+     * @brief CheckVarDeclNode check var decl node in any position.
+     * 
+     * @param decl var decl node. 
+     */
+    void CheckVarDeclNode(const shared_ptr<ast::VarDeclNode>& decl);
+
+    /**
+     * @brief CheckFuncDeclNode check func decl node in root.
+     * 
+     * @param decl func decl node.
+     */
+    void CheckFuncDeclNode(const shared_ptr<ast::FuncDeclNode>& decl);
+
+    shared_ptr<ast::FileNode> ast_;
+
     map<string, ast::FuncDeclNode> funcs_;
     map<string, ast::SingleVarDeclNode> vars_;
     shared_ptr<ec::ErrorReminder> errors_;
